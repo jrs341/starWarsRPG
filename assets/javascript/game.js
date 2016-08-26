b42ab9f9d2c4eb95b29c54672967b7e51fc85007
Jason Sinn <jrs341@me.com>
2016-08-26 11:32:25 -0500

    firing up another day

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,40 +22,35 @@ var maul = new character('Darth Maul', 180, 6, 10);
 var hero = 0;
 var villan = 0;
 
-var o = obiWan;
-var l = luke;
-var s = sidious;
-var m = maul;
+
 
 var fightSequenceArray = [];
 
 // should I embedd each function in the character object
 function attack() {
+
+	if (fightSequenceArray[0].health <= 0) {
+		console.log('You Lost');
+		return;
+	}
+	else if (fightSequenceArray[1].health <= 0) {
+		console.log('choose next opponent');
+		return;
+
+	} else {
 	var randomNum = Math.floor((Math.random() * 5)+1);
-	// var heroHealthNew = 0;
-	// var villanHealthNew =0;
+	var attack = fightSequenceArray[1].health - (fightSequenceArray[0].attackPower * randomNum);
+	fightSequenceArray[1].health = attack;
 
-	// var heroHealth = fightSequenceArray[0].health;
-	// var villanHealth = fightSequenceArray[1].health;
-	// var villan2Health = fightSequenceArray[2].health;
-	// var villan3Health = fightSequenceArray[3].helath;
-
-	// var heroAttackPower = fightSequenceArray[0].attackPower;
-	// var villancounterAttackPower = fightSequenceArray[1].counterAttackPower;
-	// var villan2counterAttackPower = fightSequenceArray[2].counterAttackPower;
-
-	var test = fightSequenceArray[1].health - (fightSequenceArray[0].attackPower * randomNum);
-	fightSequenceArray[1].health = test;
-	// fightSequenceArray.splice(fightSequenceArray[1].helath, 1, test);
-	/*if (heroHealth !== 0) {
-
-	}*/
+	var randomNum2 = Math.floor((Math.random() * 5) +1);
+	var counterAttack = fightSequenceArray[0].health - (fightSequenceArray[1].attackPower * randomNum2);
+	fightSequenceArray[0].health = counterAttack;
 
 	console.log(randomNum);
-	// console.log(fightSequenceArray[0].health);
-	// console.log(fightSequenceArray[1].helath);
-	console.log(test);
+	console.log(attack);
 	console.log(fightSequenceArray[1].health);
+	console.log(fightSequenceArray[0].health);
+	}
 }
 
 
