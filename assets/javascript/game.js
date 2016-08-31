143194675d3f2b0df10165b1b986b85825b4a7ac
Jason Sinn <jrs341@me.com>
2016-08-30 20:58:50 -0500

    added fight sequence function, but can't access object properties

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,7 +15,10 @@ var characters = [];
 characters.push(obiWan, luke, sidious, maul);
 
 var fightSequenceArray = [];
+
 var playerChoice = 0;
+var player = 0;
+var opponent = 0;
 
 function displayCharacters () {
 
@@ -32,9 +35,10 @@ $('.characterList').one('click', function() {
 	playerChoice = $('<div class = "playerChoice" >').text(this.name);
 	$('#obi').append(playerChoice);
 	fightSequenceArray.push(this);
-	displayFightSequenceArray();
+	// displayFightSequenceArray();
 	$('#buttons').attr('id', 'buttonsNone');
 	someFunction();	
+	return player = this;
 })
 
 }
@@ -53,7 +57,7 @@ function someFunction()	{
 	}
 
 	if (fightSequenceArray.length == 1) {
-		// for i = to something but not equal to element in fightSequenceArray;
+		
 		console.log("Choose Your First Opponent");
 
 		$('.characterList').one('click', function() {
@@ -62,6 +66,8 @@ function someFunction()	{
 		fightSequenceArray.push(this);
 		displayFightSequenceArray();
 		$(this).remove();
+		return opponent = this;
+		var keys = $.map(fightSequenceArray);
 		})
 	}
 }
@@ -72,6 +78,38 @@ function displayFightSequenceArray() {
 	}
 }
 
+function figthSequence() {
+
+	$('#attack').on('click', function() {
+
+	var randomNum = Math.floor((Math.random() * 5)+1);
+	var attack = opponent.health - (player.attackPower * randomNum);
+	opponent.health = attack;
+
+	if (player.health <= 0) {
+		console.log('You Lost');
+		return;
+	}
+	else if (opponent.health <= 0) {
+		fightSequenceArray.splice(1, 1);
+		console.log('choose next opponent');
+		return;
+
+	} else {
+	
+	var randomNum2 = Math.floor((Math.random() * 5) +1);
+	var counterAttack = fightSequenceArray[0].health - (fightSequenceArray[1].attackPower * randomNum2);
+	fightSequenceArray[0].health = counterAttack;
+
+	console.log(randomNum);
+	console.log(attack);
+	console.log(fightSequenceArray[1].health);
+	console.log(fightSequenceArray[0].health);
+	}
+
+	})
+}
+
 
 
 console.log(fightSequenceArray);
@@ -80,68 +118,13 @@ $(document).ready(function() {
 
 	displayCharacters();
 
-
-
-
+	figthSequence();
 
 })
 
 console.log(fightSequenceArray.length);
 
-/*if (fightSequenceArray.length != 1) {
-
-	
-
-$('#obi').one('click', function() {
-	var o = $('<button id="obi">obi</button>');
-	$('.playerChoice').append(o);
-	console.log('you chose Obi-Wan');
-	fightSequenceArray.push(obiWan);
-	console.log(fightSequenceArray);
-	return displayFightSequenceArray();
-})
-
-$('#luke').one('click', function() {
-	var l = $('<button id="luke">luke</button>');
-	$('.playerChoice').append(l);
-	console.log('you chose luke');
-	fightSequenceArray.push(luke);
-	console.log(fightSequenceArray);
-	return displayFightSequenceArray();
-})
-
-$('#sidious').one('click', function() {
-	var s = $('<button id="sidious">sidious</button>');
-	$('.playerChoice').append(s);
-	console.log('you chose sidious');
-	fightSequenceArray.push(sidious);
-	console.log(fightSequenceArray);
-	return displayFightSequenceArray();
-})
-
-	$('#maul').one('click', function() {
-	var m = $('<button id="maul">maul</button>');
-	$('.playerChoice').append(m);
-	console.log('you chose maul');
-	fightSequenceArray.push(maul);
-	console.log(fight);
-	return displayFightSequenceArray();
-})
-
-	
-
-} else {
-	console.log("working?");
-}*/
-
-
-
-
-
-
-/*function figthSequence() {
-
-	var randomNum = Math.floor((Math.random() * 5)+1);
+/*var randomNum = Math.floor((Math.random() * 5)+1);
 	var attack = fightSequenceArray[1].health - (fightSequenceArray[0].attackPower * randomNum);
 	fightSequenceArray[1].health = attack;
 
@@ -163,116 +146,8 @@ $('#sidious').one('click', function() {
 	console.log(randomNum);
 	console.log(attack);
 	console.log(fightSequenceArray[1].health);
-	console.log(fightSequenceArray[0].health);
-	}
-}*/
+	console.log(fightSequenceArray[0].health);*/
 
-// choose a player
-// simulate mouse click
-
-/*function checkKey(e) {
-
-	e = e || window.event;
-
-	if (e.keyCode == '79') {
-		console.log('you chose Obi-Wan');
-		fightSequenceArray.push(obiWan);
-		return;
-	}
-	else if (e.keyCode == '76') {
-		console.log('you chose luke');
-		fightSequenceArray.push(luke);
-		return;
-	}
-	else if (e.keyCode == '83') {
-		console.log('you chose sidious');
-		fightSequenceArray.push(sidious);
-		return;
-	} 
-	 else if (e.keyCode == '77') {
-	 	console.log('you chose maul');
-	 	fightSequenceArray.push(maul);
-	 	return;
-	}
-	else if (e.keyCode == '65') {
-		console.log('you chose to attack');
-		figthSequence();
-	} else {
-		console.log('please choose correct letter');
-	}
-	return;
-}*/
-
-
-
-
-/*function choiceSequence() {
-	
-	if (fightSequenceArray.length === 1){
-	console.log('choose your first opponent');
-	} 
-	else if (fightSequenceArray.length === 2) {
-	console.log('choose your second opponent');
-	} 
-	else if (fightSequenceArray.length === 3) {
-	console.log('last opponent');
-	} else {
-		return;
-	}
-}*/
-
-
-
-// $('')
-
-
-	
-
-// console.log('choose your character');
-
-// displayFightSequenceArray();
-
-/*$('#obi').one('click', function() {
-	var o = $('<button id="obi">obi</button>');
-	$('.chosenPlayer').append(o);
-	console.log('you chose Obi-Wan');
-	fightSequenceArray.push(obiWan);
-	
-})
-
-	$('#luke').one('click', function() {
-	var l = $('<button id="luke">luke</button>');
-	$('.chosenPlayer').append(l);
-	console.log('you chose luke');
-	fightSequenceArray.push(luke);
-})
-
-	$('#sidious').one('click', function() {
-	var s = $('<button id="sidious">sidious</button>');
-	$('.chosenPlayer').append(s);
-	console.log('you chose sidious');
-	fightSequenceArray.push(sidious);
-})
-
-	$('#maul').one('click', function() {
-	var m = $('<button id="maul">maul</button>');
-	$('.chosenPlayer').append(m);
-	console.log('you chose maul');
-	fightSequenceArray.push(maul);
-})
-
-
-
-// document.onkeydown = function(event) {
-
-// checkKey();
-
-
-
-// choiceSequence();
-
-
-})*/
 
 
 
